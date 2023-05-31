4cb3d986c0b9e7262f22a3e10de6f5237578a674
taeyoung0504 <taeyoung050405@gmail.com>
2023-05-31 18:57:53 +0900

    ID, PW 찾기 구현

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
 
 import com.mysite.sbb.email.EmailService;
 import com.mysite.sbb.email.RedisUtill;
@@ -77,7 +78,7 @@ public class UserController {
 
 	private final EmailService emailService;
 	private final PasswordEncoder passwordEncoder;
-	
+
 	@PostMapping("/emailcode")
 	@ResponseBody
 	public String mailConfirm(@RequestParam String email) throws Exception {
@@ -93,22 +94,56 @@ public class UserController {
 	@PostMapping("/temp_pwd")
 	@ResponseBody
 	public String sendTempPwd(@RequestParam String email, @RequestParam String username) throws Exception {
-	
+
 		List<SiteUser> users = userService.find(username, email);
-		
-		if(users !=null && !users.isEmpty()) {
+
+		if (users != null && !users.isEmpty()) {
 			String code = emailService.sendSimpleMessage(email);
-		
-			//임시 비밀번호로 패스워드 변경
-			  	SiteUser user = users.get(0);
-			  	user.setPassword(passwordEncoder.encode(code));
-		        userService.save(user);
-			
+
+			// 임시 비밀번호로 패스워드 변경
+			SiteUser user = users.get(0);
+			user.setPassword(passwordEncoder.encode(code));
+			userService.save(user);
+
 			return code;
 		} else {
 			String result = null;
 			return result;
 		}
-	
+
 	}
+
+	@GetMapping("/find_id")
+	public String findID() {
+		return "find_id";
+	}
+
+	@PostMapping("/find_id")
+	@ResponseBody
+	public String findid(@RequestParam String email) {
+		Optional<SiteUser> users = userService.check2(email);
+
+		if (users.isPresent()) {
+			SiteUser user = users.get();
+			String username = user.getUsername();
+			if (username != null) {
+				int length = username.length();
+				int maskedLength = Math.min(length, 6);
+				String maskedUsername = username.substring(0, length - maskedLength)
+						+ StringUtils.repeat("*", maskedLength);
+
+				String result2 = "<br><br>전체 아이디를 알고자 하시는 분은 고객센터로 문의 바랍니다.";
+				return "<br>고객 ID :  " + maskedUsername + "<br> 계정 보안을 위해 마스킹 된 정보만 제공합니다" + result2;
+			} else {
+				String result = "<br>등록된 사용자가 아니거나 SNS 유저는 확인 할 수 없습니다.<br>";
+
+				return result;
+			}
+		} else {
+			String result = "<br>등록된 사용자가 아니거나 SNS 유저는 확인 할 수 없습니다.<br>";
+
+			return result;
+		}
+	}
+
 }
